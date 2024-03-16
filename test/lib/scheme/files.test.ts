8b5698c82aba4f73b1f72aa6beb28390c9d3392d
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2024-03-16 17:10:47 +0900

    feat: 파일 관련 스키마 변경 및 추가

--- test/lib/scheme/files.test.ts
+++ test/lib/scheme/files.test.ts
@@ -1,6 +1,6 @@
 import knex, { Knex } from 'knex';
 import {beforeAll, afterAll, beforeEach, afterEach, describe, expect, it} from 'vitest';
-import { createFileTable, Files, batchInsertFiles } from '../../../src/lib/scheme/files';
+import { createFilesTable, createFileTagsTable, createTagsTable, Files, Tags} from '../../../src/lib/utils/scheme/files';
 
 describe("createFileTable", () => {
     let db: Knex;
@@ -14,15 +14,15 @@ describe("createFileTable", () => {
             useNullAsDefault: true,
         });
 
-        await createFileTable(db);
+        await createFilesTable(db);
     });
 
-    it(`${Files} 이름을 가진 테이블이 생성되어야 한다`, async () => {
+    it(`files 이름을 가진 테이블이 생성되어야 한다`, async () => {
         const hasTable = await db.schema.hasTable(Files);
         expect(hasTable).toBe(true);
     });
 
-    it(`${Files} 테이블은 id, filePath, urlPath, fileType, metadata 컬럼을 가져야 한다`, async () => {
+    it(`files 테이블은 id, filePath, urlPath, fileType, metadata 컬럼을 가져야 한다`, async () => {
         const promise: Promise<boolean>[] = [];
         promise.push(db.schema.hasColumn(Files, "id"));
         promise.push(db.schema.hasColumn(Files, "filePath"));
@@ -46,10 +46,11 @@ describe("createFileTable", () => {
     });
 });
 
-describe("batchInsertFiles", () => {
+
+describe("createTagsTable", () => {
     let db: Knex;
 
-    beforeEach(async () => {
+    beforeAll(async () => {
         db = knex({
             client: "sqlite3",
             connection: {
@@ -58,66 +59,59 @@ describe("batchInsertFiles", () => {
             useNullAsDefault: true,
         });
 
-        await createFileTable(db);
+        await createTagsTable(db);
     });
 
-    it("입력할 파일이 CHUNK_SIZE 단위보다 작은 경우 한번에 모두 입력되어야 한다 ", async () => {
-        const files = [
-            {
-                filePath: "file1",
-                urlPath: "url1",
-                fileType: "type1",
-                metadata: "metadata1",
-            },
-            {
-                filePath: "file2",
-                urlPath: "url2",
-                fileType: "type2",
-                metadata: "metadata2",
-            },
-        ];
-        batchInsertFiles(db, files);
-        const result = await db.select("*").from(Files);
-        expect(result).toEqual(
-            files.map((file, idx) => ({ id: idx + 1, ...file }))
-        );
+    it(`tags 이름을 가진 테이블이 생성되어야 한다`, async () => {
+        const hasTable = await db.schema.hasTable(Tags);
+        expect(hasTable).toBe(true);
     });
 
-    it("입력할 파일이 CHUNK_SIZE 단위보다 큰 경우 CHUNK_SIZE 단위로 나눠서 입력되어야 한다 ", async () => {
-        const files = [
-            {
-                filePath: "file1",
-                urlPath: "url1",
-                fileType: "type1",
-                metadata: "metadata1",
-            },
-            {
-                filePath: "file2",
-                urlPath: "url2",
-                fileType: "type2",
-                metadata: "metadata2",
-            },
-            {
-                filePath: "file3",
-                urlPath: "url3",
-                fileType: "type3",
-                metadata: "metadata3",
-            },
-            {
-                filePath: "file4",
-                urlPath: "url4",
-                fileType: "type4",
-                metadata: "metadata4",
-            },
-        ];
-        batchInsertFiles(db, files, 2);
-        const result = await db.select("*").from(Files);
-        expect(result).toEqual(
-            files.map((file, idx) => ({ id: idx + 1, ...file }))
-        );
+    it(`tags 테이블은 id, name 컬럼을 가져야 한다`, async () => {
+        const promise: Promise<boolean>[] = [];
+        promise.push(db.schema.hasColumn(Tags, "id"));
+        promise.push(db.schema.hasColumn(Tags, "name"));
+        const [id, name] = await Promise.all(promise);
+        expect([id, name]).toEqual([true, true]);
     });
 
-    afterEach(async () => {
+    afterAll(async () => {
+        await db.destroy();
+    });
+});
+
+describe("createFileTagsTable", () => {
+    let db: Knex;
+
+    beforeAll(async () => {
+        db = knex({
+            client: "sqlite3",
+            connection: {
+                filename: ":memory:",
+            },
+            useNullAsDefault: true,
+        });
+
+        await createTagsTable(db);
+        await createFilesTable(db);
+        await createFileTagsTable(db);
+    });
+
+    it(`fileTags 이름을 가진 테이블이 생성되어야 한다`, async () => {
+        const hasTable = await db.schema.hasTable("fileTags");
+        expect(hasTable).toBe(true);
+    });
+
+    it(`fileTags 테이블은 id, fileId, tagId 컬럼을 가져야 한다`, async () => {
+        const promise: Promise<boolean>[] = [];
+        promise.push(db.schema.hasColumn("fileTags", "id"));
+        promise.push(db.schema.hasColumn("fileTags", "fileId"));
+        promise.push(db.schema.hasColumn("fileTags", "tagId"));
+        const [id, fileId, tagId] = await Promise.all(promise);
+        expect([id, fileId, tagId]).toEqual([true, true, true]);
+    });
+
+    afterAll(async () => {
         await db.destroy();
     });
 });
