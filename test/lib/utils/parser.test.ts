7bc3f0797aa531277e6d6a31ce8db882a9b02216
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2024-03-18 13:42:11 +0900

    test: 테스트 파일 수정

--- test/lib/utils/parser.test.ts
+++ test/lib/utils/parser.test.ts
@@ -4,14 +4,14 @@ import {visit} from "unist-util-visit";
 
 describe("buildAst", () => {
     it("should parse markdown content", () => {
-        const result = buildAst("# Hello World");
+        const result = buildAst("# Hello World") as any;
         visit(result, "heading", (node, index, parent) => {
             expect(node.children[0].value).toBe("Hello World");
         });
     });
 
     it("should parse markdown content with wiki links", () => {
-        const result = buildAst("# Hello World [[link]]");
+        const result = buildAst("# Hello World [[link]]") as any; // Add type assertion
         // console.log(JSON.stringify(result, null, 2));
         visit(result, "wikiLink", (node, index, parent) => {
             expect(node.data.hName).toBe("a");
@@ -22,7 +22,7 @@ describe("buildAst", () => {
     it("shold parse markdown content with wiki links and permalinks", () => {
         const result = buildAst("# Hello World [[link]]", {
             permalinks: ["content/a/link"]
-        });
+        }) as any;
         visit(result, "wikiLink", (node, index, parent) => {
             expect(node.data.hProperties.href).toBe("content/a/link");
         });
