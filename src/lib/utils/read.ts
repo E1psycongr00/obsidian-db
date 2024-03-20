3d540cb56ceb27d53c844da7a8755c474a8f140b
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2024-03-20 22:04:09 +0900

    feat: directory를 읽어서 batch하는 함수 추가

---
+++ src/lib/utils/read.ts
@@ -0,0 +1,55 @@
+import * as fs from "fs";
+import { findFilesAll } from "./filePath";
+import { parseFile } from "./parser";
+import { File } from "./scheme/files";
+import { batchInsertFiles } from "./query/fileQuery";
+import { batchInsertLinks } from "./query/linkQuery";
+import { Knex } from "knex";
+import { Link } from "./scheme/links";
+
+
+async function batchInsertDirectories(db:Knex, dir: string) {
+    const filePaths = findFilesAll(dir);
+    const {files, titleLinks} = transFiles(filePaths);
+    await batchInsertFiles(db, files);
+    const fileData = await db.select("id", "urlPath").from("files");
+    const links = titleLinks.map(link => {
+        const sourceId: number = fileData.find(f => f.urlPath === link.sourceTitle)?.id;
+        const targetId: number = fileData.find(f => f.urlPath === link.targetTitle)?.id;
+        const type: "normal" | "embed" = link.linkType || "normal";
+        return {
+            sourceFileId: sourceId,
+            targetFileId: targetId,
+            type: type,
+        } as Link;
+    
+    });
+    await batchInsertLinks(db, links);
+}
+
+function transFiles(filePaths: string[]) {
+    const files = [];
+    const titleLinks = [];
+    for (const filePath of filePaths) {
+        const file = fs.readFileSync(filePath, "utf8");
+        const { metaData: metadata, links: fileLinks } = parseFile(file);
+        const { urlPath, fileType } = splitFilePath(filePath);
+        files.push({
+            filePath,
+            urlPath,
+            fileType,
+            metadata,
+        } as File);
+        titleLinks.push(...fileLinks);
+    }
+    return { files, titleLinks };
+}
+
+function splitFilePath(filePath: string) {
+    const split = filePath.split(".");
+    const fileType = split[split.length - 1];
+    const urlPath = split.slice(0, split.length - 1).join(".");
+    return { urlPath, fileType };
+}
+
+export {batchInsertDirectories}
