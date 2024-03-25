342c8abec8f933b6971133d69ea33bbde420fb68
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2024-03-25 12:07:19 +0900

    fix: default parseOption을 추가

--- src/lib/utils/read.ts
+++ src/lib/utils/read.ts
@@ -1,38 +1,51 @@
 import * as fs from "fs";
 import { findFilesAll } from "./filePath.js";
-import { parseFile } from "./parser.js";
+import { ParseOptions, parseFile } from "./parser.js";
 import { File } from "./scheme/files.js";
 import { batchInsertFiles } from "./query/fileQuery.js";
 import { batchInsertLinks } from "./query/linkQuery.js";
 import { Knex } from "knex";
 import { Link } from "./scheme/links.js";
 
-
-async function batchInsertDirectories(db:Knex, dir: string) {
+async function batchInsertDirectories(
+    db: Knex,
+    dir: string,
+    parseOptions: ParseOptions = {}
+) {
     const filePaths = findFilesAll(dir);
-    const {files, titleLinks} = transFiles(filePaths);
+    if (!parseOptions.buildAstOptions) {
+        const permalinks = filePaths.map(filePath => {
+            const { urlPath } = splitFilePath(filePath);
+            return urlPath;
+        });
+        parseOptions.buildAstOptions = { permalinks };
+    }
+    const { files, titleLinks } = transFiles(filePaths, parseOptions);
     await batchInsertFiles(db, files);
     const fileData = await db.select("id", "urlPath").from("files");
     const links = titleLinks.map(link => {
-        const sourceId: number = fileData.find(f => f.urlPath === link.sourceTitle)?.id;
-        const targetId: number = fileData.find(f => f.urlPath === link.targetTitle)?.id;
+        const sourceId: number = fileData.find(
+            f => f.urlPath === link.sourceTitle
+        )?.id;
+        const targetId: number = fileData.find(
+            f => f.urlPath === link.targetTitle
+        )?.id;
         const type: "normal" | "embed" = link.linkType || "normal";
         return {
             sourceFileId: sourceId,
             targetFileId: targetId,
             type: type,
         } as Link;
-    
     });
     await batchInsertLinks(db, links);
 }
 
-function transFiles(filePaths: string[]) {
+function transFiles(filePaths: string[], parseOptions?: ParseOptions) {
     const files = [];
     const titleLinks = [];
     for (const filePath of filePaths) {
         const file = fs.readFileSync(filePath, "utf8");
-        const { metaData: metadata, links: fileLinks } = parseFile(file);
+        const { metaData: metadata, links: fileLinks} = parseFile(file, parseOptions);
         const { urlPath, fileType } = splitFilePath(filePath);
         files.push({
             filePath,
@@ -52,4 +65,4 @@ function splitFilePath(filePath: string) {
     return { urlPath, fileType };
 }
 
-export {batchInsertDirectories}
+export { batchInsertDirectories };
