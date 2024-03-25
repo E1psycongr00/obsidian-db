87480ddc94e6861a5c8dde8b1e4a5ab544f7981a
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2024-03-25 19:29:22 +0900

    feat: read에서 link 실패시 로그 출력

--- src/lib/utils/read.ts
+++ src/lib/utils/read.ts
@@ -5,11 +5,7 @@ import { batchInsertLinks } from "./query/linkQuery.js";
 import { Knex } from "knex";
 import { Link } from "./scheme/links.js";
 
-async function batchInsertDirectories(
-    db: Knex,
-    dir: string,
-    parser: Parser
-) {
+async function batchInsertDirectories(db: Knex, dir: string, parser: Parser) {
     const filePaths = findFilePathsAll(dir);
     const { files, urlLinks } = transFiles(filePaths, parser);
     await batchInsertFiles(db, files);
@@ -22,7 +18,7 @@ function transFiles(filePaths: string[], parser: Parser) {
     const files = [];
     const urlLinks = [];
     for (const filePath of filePaths) {
-        const {file, links} = parser.parseFile(filePath);
+        const { file, links } = parser.parseFile(filePath);
         files.push(file);
         urlLinks.push(...links);
     }
@@ -30,20 +26,35 @@ function transFiles(filePaths: string[], parser: Parser) {
 }
 
 function convertIdLinks(fileData: any[], urlLinks: UrlLink[]) {
-    return urlLinks.map(link => {
-        const sourceId: number = fileData.find(
-            f => f.urlPath === link.source
-        ).id;
-        const targetId: number = fileData.find(
-            f => f.urlPath === link.target
-        ).id;
-        const type: "normal" | "embed" = link.linkType || "normal";
-        return {
-            sourceFileId: sourceId,
-            targetFileId: targetId,
-            type: type,
-        } as Link;
-    });
+    const links = [];
+    for (const urlLink of urlLinks) {
+        try {
+            links.push(convertLink(fileData, urlLink));
+        } catch (error) {
+            console.error(error + "so disconnect link");   
+        }
+    }
+    return links;
+}
+
+function convertLink(
+    fileData: { id: number; urlPath: string }[],
+    link: UrlLink
+) {
+    const source = fileData.find(f => f.urlPath === link.source);
+    if (!source) {
+        throw new Error(`source file not found. link source: ${link.source}`);
+    }
+    const target = fileData.find(f => f.urlPath === link.target);
+    if (!target) {
+        throw new Error(`target file not found. link target: ${link.target}`);
+    }
+    const type: "normal" | "embed" = link.linkType || "normal";
+    return {
+        sourceFileId: source.id,
+        targetFileId: target.id,
+        type: type,
+    } as Link;
 }
 
 export { batchInsertDirectories };
