7ea299dc7484bf91bb61012922557741d17ab63f
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2024-03-25 18:41:52 +0900

    refactor: read의 일부 역학을 parser에 위임하고 parser 주입

--- src/lib/utils/read.ts
+++ src/lib/utils/read.ts
@@ -1,7 +1,5 @@
-import * as fs from "fs";
 import { findFilePathsAll } from "./filePath.js";
-import { ParseOptions, parseFile } from "./parser.js";
-import { File } from "./scheme/files.js";
+import Parser, { UrlLink } from "./parser.js";
 import { batchInsertFiles } from "./query/fileQuery.js";
 import { batchInsertLinks } from "./query/linkQuery.js";
 import { Knex } from "knex";
@@ -10,26 +8,35 @@ import { Link } from "./scheme/links.js";
 async function batchInsertDirectories(
     db: Knex,
     dir: string,
-    parseOptions: ParseOptions = {}
+    parser: Parser
 ) {
     const filePaths = findFilePathsAll(dir);
-    if (!parseOptions.buildAstOptions) {
-        const permalinks = filePaths.map(filePath => {
-            const { urlPath } = splitFilePath(filePath);
-            return urlPath;
-        });
-        parseOptions.buildAstOptions = { permalinks };
-    }
-    const { files, titleLinks } = transFiles(filePaths, parseOptions);
+    const { files, urlLinks } = transFiles(filePaths, parser);
     await batchInsertFiles(db, files);
     const fileData = await db.select("id", "urlPath").from("files");
-    const links = titleLinks.map(link => {
+    const links = convertIdLinks(fileData, urlLinks);
+    await batchInsertLinks(db, links);
+}
+
+function transFiles(filePaths: string[], parser: Parser) {
+    const files = [];
+    const urlLinks = [];
+    for (const filePath of filePaths) {
+        const {file, links} = parser.parseFile(filePath);
+        files.push(file);
+        urlLinks.push(...links);
+    }
+    return { files, urlLinks };
+}
+
+function convertIdLinks(fileData: any[], urlLinks: UrlLink[]) {
+    return urlLinks.map(link => {
         const sourceId: number = fileData.find(
             f => f.urlPath === link.source
-        )?.id;
+        ).id;
         const targetId: number = fileData.find(
             f => f.urlPath === link.target
-        )?.id;
+        ).id;
         const type: "normal" | "embed" = link.linkType || "normal";
         return {
             sourceFileId: sourceId,
@@ -37,35 +44,6 @@ async function batchInsertDirectories(
             type: type,
         } as Link;
     });
-    await batchInsertLinks(db, links);
-}
-
-function transFiles(filePaths: string[], parseOptions?: ParseOptions) {
-    const files = [];
-    const titleLinks = [];
-    for (const filePath of filePaths) {
-        const file = fs.readFileSync(filePath, "utf8");
-        const { metaData: metadata, links: fileLinks } = parseFile(
-            file,
-            parseOptions
-        );
-        const { urlPath, fileType } = splitFilePath(filePath);
-        files.push({
-            filePath,
-            urlPath,
-            fileType,
-            metadata,
-        } as File);
-        titleLinks.push(...fileLinks);
-    }
-    return { files, titleLinks };
-}
-
-function splitFilePath(filePath: string) {
-    const split = filePath.split(".");
-    const fileType = split[split.length - 1];
-    const urlPath = split.slice(0, split.length - 1).join(".");
-    return { urlPath, fileType };
 }
 
 export { batchInsertDirectories };
---
+++ test/lib/utils/read.test.ts
@@ -0,0 +1,79 @@
+import knex, { Knex } from "knex";
+import { batchInsertDirectories } from "../../../src/lib/utils/read";
+import { describe, it, expect, beforeAll, afterAll } from "vitest";
+import {
+    createFileTagsTable,
+    createFilesTable,
+    createTagsTable,
+} from "../../../src/lib/utils/scheme/files";
+import { createLinkTable } from "../../../src/lib/utils/scheme/links";
+import path from "path";
+import Parser from "../../../src/lib/utils/parser";
+import { findFilePathsAll } from "../../../src/lib/utils/filePath";
+
+describe("batchInsertDirectories", () => {
+    let db: Knex;
+
+    beforeAll(async () => {
+        db = knex({
+            client: "sqlite3",
+            connection: {
+                filename: ":memory:",
+            },
+            useNullAsDefault: true,
+        });
+        await createFilesTable(db);
+        await createTagsTable(db);
+        await createFileTagsTable(db);
+        await createLinkTable(db);
+    });
+
+    afterAll(async () => {
+        await db.destroy();
+    });
+
+    it("should insert directories and files into the database", async () => {
+        const dir = path.join(process.cwd(), "test", "__mock__", "contents");
+        const parser = new Parser();
+        await batchInsertDirectories(db, dir, parser);
+
+        const fileCount = await db.count().from("files");
+        expect(fileCount[0]["count(*)"]).toBe(6);
+    });
+
+    it("should insert links between files into the database", async () => {
+        const dir = path.join(process.cwd(), "test", "__mock__", "contents");
+        const permalinks = findFilePathsAll(dir).map(
+            filePath => splitFilePath(filePath).urlPath
+        );
+        const parser = new Parser({ permalinks });
+        await batchInsertDirectories(db, dir, parser);
+
+        const linkCount = await db.count().from("links");
+        expect(linkCount[0]["count(*)"]).toBe(2);
+    });
+
+    it("should insert tags into the database", async () => {
+        const dir = path.join(process.cwd(), "test", "__mock__", "contents");
+        const permalinks = findFilePathsAll(dir).map(
+            filePath => splitFilePath(filePath).urlPath
+        );
+        const parser = new Parser({ permalinks });
+        await batchInsertDirectories(db, dir, parser);
+        const tagCount = await db.count().from("tags");
+        const tagNames = await db("tags").select("name");
+        expect(tagCount[0]["count(*)"]).toBe(4);
+        expect(tagNames.map((tag: { name: string }) => tag.name))
+            .contain("tag1")
+            .contain("tag2")
+            .contain("tag3")
+            .contain("tag4");
+    });
+
+    function splitFilePath(filePath: string) {
+        const split = filePath.split(".");
+        const fileType = split[split.length - 1];
+        const urlPath = split.slice(0, split.length - 1).join(".");
+        return { urlPath, fileType };
+    }
+});
