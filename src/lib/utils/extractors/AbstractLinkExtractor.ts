00c12ddfb0eac0bd94a7d0aeeff773b92d8033c6
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2024-03-27 22:43:31 +0900

    feat: abstractLinkExtractor.ts 파일 추가

---
+++ src/lib/utils/extractors/AbstractLinkExtractor.ts
@@ -0,0 +1,24 @@
+import { Root } from "remark-gfm";
+import { visit } from "unist-util-visit";
+import LinkExtractor from "./linkExtractor";
+
+abstract class AbstractLinkExtractor implements LinkExtractor {
+    private readonly linkType: string;
+
+    constructor(linkType: string) {
+        this.linkType = linkType;
+    }
+
+    public extract(ast: Root): string[] {
+        const targetLinks: string[] = [];
+        visit(ast, this.linkType, (node: any) => {
+            const targetLink = this.customExtractLogic(node);
+            targetLinks.push(targetLink);
+        });
+        return targetLinks;
+    }
+
+    protected abstract customExtractLogic(node: any): string;
+}
+
+export default AbstractLinkExtractor;
