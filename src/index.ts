9cd5cc89120b967abc5d376bd839b7bafa53afdd
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2024-03-28 19:56:21 +0900

    feat: link extractor 인터페이스 제공

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { ObsidianDbBuilder, DbConfig } from "./lib/utils/obsidianDB.js";
 import ObsidianDb from "./lib/utils/obsidianDB.js";
+import AbstractLinkExtractor from "./lib/utils/extractors/AbstractLinkExtractor.js";
+import LinkExtractor from "./lib/utils/extractors/linkExtractor.js";
 
 export default ObsidianDb;
-export { ObsidianDbBuilder, DbConfig };
+export { ObsidianDbBuilder, DbConfig, AbstractLinkExtractor, LinkExtractor };
