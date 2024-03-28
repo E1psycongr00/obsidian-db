48320d179cc7095180d6c93cf3d0b0564cbb71df
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2024-03-28 11:55:47 +0900

    feat: image link extractor 추가

---
+++ test/lib/utils/extractors/ImageLinkExtractor.test.ts
@@ -0,0 +1,13 @@
+import ImageWikiLinkExtractor from "../../../../src/lib/utils/extractors/ImageWikiLinkExtractor";
+import Parser from "../../../../src/lib/utils/Parser";
+import { describe, it } from "vitest";
+
+describe("ImageLinkExtractor", () => {
+    it("should extract image link", () => {
+        const extractor = new ImageWikiLinkExtractor();
+        const parser = new Parser({permalinks: ["imageLink"]});
+        const ast = parser.parseAst("![[imageLink]]");
+        const links = extractor.extract(ast);
+        console.log(links);
+    });
+});
