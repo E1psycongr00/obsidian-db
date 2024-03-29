9f366a5f68f2b2564381e07933e6324e4792138e
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2024-03-29 18:14:27 +0900

    feat: path와 permalink 유틸 함수 인터페이스 제공

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ import { ObsidianDbBuilder, DbConfig } from "./lib/utils/obsidianDB.js";
 import ObsidianDb from "./lib/utils/obsidianDB.js";
 import AbstractLinkExtractor from "./lib/utils/extractors/AbstractLinkExtractor.js";
 import LinkExtractor from "./lib/utils/extractors/linkExtractor.js";
+import {
+    findFilePathsAll,
+    findPermalinksAll,
+    toPermalink,
+    fixWindowPath,
+} from "./lib/utils/permalinks/filePath.js";
 
 export default ObsidianDb;
 export { ObsidianDbBuilder, DbConfig, AbstractLinkExtractor, LinkExtractor };
+export { findFilePathsAll, findPermalinksAll, toPermalink, fixWindowPath };
