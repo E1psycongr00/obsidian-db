babea759d06cc741af192b0e1f3b4d2e19e2746c
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2024-03-29 18:11:29 +0900

    feat: findPermalink 추가 및 코드 개선
    - findPermalinksAll 추가
    - 인자에 extension이 추가되어 필터링해서 검색 가능함

--- test/lib/utils/permalinks/filePath.test.ts
+++ test/lib/utils/permalinks/filePath.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { findFilePathsAll } from "../../../../src/lib/utils/permalinks/filePath";
+import { findFilePathsAll, findPermalinksAll } from "../../../../src/lib/utils/permalinks/filePath";
 import * as path from "path";
 
 describe("findFilesAll", () => {
@@ -9,10 +9,50 @@ describe("findFilesAll", () => {
         expect(files).toEqual([
             "test/__mock__/contents/A.md",
             "test/__mock__/contents/B.md",
-            "test/__mock__/contents/dep1a/C.md",
+            "test/__mock__/contents/dep1a/C.mdx",
             "test/__mock__/contents/dep1a/D.md",
             "test/__mock__/contents/dep1a/dep2a/F.md",
             "test/__mock__/contents/dep1b/E.md",
         ]);
+    });
+
+    it("extension 입력이 주어진 경우 해당 확장자 파일만 반환해야 함", () => {
+        const rootDir = path.join("./test/__mock__", "contents");
+        const files = findFilePathsAll(rootDir, ["mdx"]);
+        expect(files).toEqual(["test/__mock__/contents/dep1a/C.mdx"]);
+    });
+
+    it("extension 입력이 여러개 주어진 경우 해당 확장자 파일만 반환해야 함", () => {
+        const rootDir = path.join("./test/__mock__", "contents");
+        const files = findFilePathsAll(rootDir, ["mdx", "md"]);
+        expect(files).toEqual([
+            "test/__mock__/contents/A.md",
+            "test/__mock__/contents/B.md",
+            "test/__mock__/contents/dep1a/C.mdx",
+            "test/__mock__/contents/dep1a/D.md",
+            "test/__mock__/contents/dep1a/dep2a/F.md",
+            "test/__mock__/contents/dep1b/E.md",
+        ]);
+    });
+});
+
+describe("findPermalinksAll", () => {
+    it("자식 디렉토리 파일 permalink를 반환해야 함", () => {
+        const rootDir = "test/__mock__/contents";
+        const permalinks = findPermalinksAll(rootDir);
+        expect(permalinks).toEqual([
+            "A",
+            "B",
+            "dep1a/C",
+            "dep1a/D",
+            "dep1a/dep2a/F",
+            "dep1b/E",
+        ]);
+    });
+
+    it("extension 입력이 주어진 경우 해당 확장자 파일 permalink만 반환해야 함", () => {
+        const rootDir = "test/__mock__/contents";
+        const permalinks = findPermalinksAll(rootDir, ["mdx"]);
+        expect(permalinks).toEqual(["dep1a/C"]);
     });
 });
